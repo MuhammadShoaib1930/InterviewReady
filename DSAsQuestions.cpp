b7397e87d3ffb5a80cdbf2525a2319223991510b
Muhammad shoaib <shoaibsn54321@gmail.com>
2026-05-31 11:33:12 +0500

    solved array probleam

--- DSAsQuestions.cpp
+++ DSAsQuestions.cpp
@@ -3,11 +3,12 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector<int> inputVector(int n) {
+vector<int> inputVector() {
     vector<int> result;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; true; i++) {
         int s;
         cin >> s;
+        if (s == -1000) break;
         result.push_back(s);
     }
     return result;
@@ -243,14 +244,233 @@ vector<int> productExceptSelf2(vector<int> arr) {
     }
     return sufix;
 }
+int maxProduct(vector<int> arr) {
+    int maxsub = INT_MIN;
+    int prod = 1;
+    for (int i = 0; i < arr.size(); i++) {
+        prod *= arr[i];
+        maxsub = max(maxsub, prod);
+        if (prod == 0) {
+            prod = 1;
+        }
+    }
+    return maxsub;
+}
+int findMin(vector<int> arr) {
+    int n = arr.size() - 1;
+    if (arr[0] < arr[n - 1]) return arr[0];
+    int st = 0, ed = n, mid = 0;
+    while (st <= ed) {
+        mid = st + (ed - st) / 2;
+        if (arr[mid - 1] > arr[mid] && arr[mid] < arr[mid + 1]) {
+            return mid;
+        } else if (arr[st] < arr[mid]) {
+            st = mid + 1;
+        } else {
+            ed = mid - 1;
+        }
+    }
+    return mid;
+}
+
+bool PairSumSortedAndRotated(vector<int> arr, int target) {
+    int mid = findMin(arr);
+    int st = mid;
+    int ed = mid - 1;
+    while (st != ed) {
+        int temp = arr[st] + arr[ed];
+
+        if (temp == target) {
+            return true;
+        } else if (temp > target) {
+            ed--;
+            if (ed < 0) ed = arr.size() - 1;
+        } else if (temp < target) {
+            st++;
+            if (st > arr.size() - 1) st = 0;
+        }
+    }
+
+    return false;
+}
+void threeSum(vector<int> nums) {
+    vector<vector<int>> res;
+    for (int i = 0; i < nums.size() - 2; i++) {
+        for (int j = i + 1; j < nums.size() - 1; j++) {
+            for (int k = j + 1; k < nums.size(); k++) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    vector<int> temp;
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(nums[k]);
+                    res.push_back(temp);
+                }
+            }
+        }
+    }
+    for (int i = 0; i < res.size(); i++) {
+        printVector(res[i]);
+    }
+}
+void threeSum2(vector<int> nums) {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> resl;
+    for (int i = 0; i < n - 2; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        int j = i + 1, k = n - 1;
+        while (j < k) {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum < 0) {
+                j++;
+            } else if (sum > 0) {
+                k--;
+            } else {
+                resl.push_back({nums[i], nums[j], nums[k]});
+                k--;
+                j++;
+                while (j < k && nums[j] == nums[j - 1]) j++;
+                while (j < k && nums[k] == nums[k + 1]) k--;
+            }
+        }
+    }
+    for (int i = 0; i < resl.size() - 1; i++) {
+        int freq = 0;
+        for (int j = 0; j < resl[i].size(); j++) {
+            if (resl[i][j] == resl[i + 1][j]) {
+                freq++;
+            }
+            if (freq == 3) {
+                resl.erase(resl.begin() + i);
+                i--;
+            }
+        }
+    }
+
+    for (int i = 0; i < resl.size(); i++) {
+        printVector(resl[i]);
+    }
+}
+int ContainerWithMostWater(vector<int> height) {
+    int i = 0, j = height.size() - 1;
+    int maxWater = INT_MIN;
+    while (i < j) {
+        int area = (j - i) * min(height[i], height[j]);
+        maxWater = max(maxWater, area);
+        if (height[i] < height[j]) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+    return maxWater;
+}
+int kthSmallest(vector<int> arr, int k) {
+    if (k > arr.size() - 1) return -1;
+    if (k == arr.size() - 1) return arr[arr.size() - 1];
+    sort(arr.begin(), arr.end());
+    return arr[k - 1];
+}
+int kthSmallest2(vector<int> arr, int k) {
+    if (k > arr.size() || k <= 0) return -1;
+    priority_queue<int> heap;
+    for (int i = 0; i < arr.size(); i++) {
+        heap.push(arr[i]);
+        if (heap.size() > k) {
+            heap.pop();
+        }
+    }
+    return heap.top();
+}
+vector<vector<int>> input2DVector() {
+    int n = 0, m = 0;
+    cin >> n;
+    vector<vector<int>> inp;
+    for (int i = 0; i < n; i++) {
+        cin >> m;
+        vector<int> temp;
+        for (int j = 0; j < m; j++) {
+            int s = 0;
+            cin >> s;
+            temp.push_back(s);
+        }
+        inp.push_back(temp);
+    }
+    return inp;
+}
+void print2DVector(vector<vector<int>> arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = 0; j < arr[i].size(); j++) {
+            cout << arr[i][j];
+            if (j + 1 < arr[i].size()) {
+                cout << " , ";
+            }
+        }
+        cout << endl;
+    }
+    cout << endl << endl;
+}
+vector<vector<int>> mergeOverlappinIntervals(vector<vector<int>> arr) {
+    if (arr.size() < 2) return arr;
+    sort(arr.begin(), arr.end());
+    int n = arr.size();
+    int i = 1;
+    vector<vector<int>> res;
+    int f = arr[0][0], l = arr[0][1];
+    for (int i = 1; i < n; i++) {
+        if (arr[i][0] <= l) {
+            l = max(l, arr[i][1]);
+        } else {
+            res.push_back({f, l});
+            f = arr[i][0], l = arr[i][1];
+        }
+    }
+    res.push_back({f, l});
+
+    return res;
+}
+int FindMinimumNumberOfMergeOperationsToMakeAnArrayPalindrome(vector<int> arr) {
+    if (arr.size() < 2) return 0;
+    int i = 0, j = arr.size() - 1;
+    int opration = 0;
+    while (i < j) {
+        if (arr[i] == arr[j]) {
+            i++;
+            j--;
+        } else if (arr[i] < arr[i + 1]) {
+            opration++;
+            arr[i + 1] += arr[i];
+            i++;
+        } else {
+            opration++;
+            arr[j - 1] += arr[j];
+
+            j--;
+        }
+    }
+    return opration;
+}
+bool myCompare(const string& a, const string& b) { return (a + b) > (b + a); }
+string CreateLargestConcatenatedNumber(vector<int> arr) {
+    if (arr.size() < 1) return "";
+    if (arr.size() < 2) return to_string(arr[0]);
+    vector<string> starr;
+    for (int i = 0; i < arr.size(); i++) {
+        starr.push_back(to_string(arr[i]));
+    }
+    sort(starr.begin(), starr.end(), myCompare);
+    if (starr[0] == "0") return "0";
+    string res = "";
+    for (string item : starr) {
+        res += item;
+    }
+    return res;
+}
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    int n = 0;
-    cin >> n;
-    vector<int> input = inputVector(n);
-    vector<int> outpu = productExceptSelf2(input);
-    printVector(outpu);
+    vector<int> input = inputVector();
+    cout << CreateLargestConcatenatedNumber(input);
 
     return 0;
 }
